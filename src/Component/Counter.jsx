fefd3453b7fa6706cf43d92db457f2729f5d4043
balajiinfo8 <vinothkumarbalaji150@gmail.com>
2025-10-28 15:26:44 +0530

    Fresh start

---
+++ src/Component/Counter.jsx
@@ -0,0 +1,39 @@
+// useState for dynamic useage 
+import React , {useState} from 'react';
+// Counter Function to Count Number 
+function Counter() 
+{
+     // declare 'useState' in counter 
+        const [count , setCount] = useState(0);
+        
+        const increment = () => setCount(count + 1)
+
+        const decrement = () => setCount(count -  1)
+
+        const reset = () => setCount(0)
+
+        // automatic click 
+        const mouseEnter = (e) => {
+            e.target.click();
+        }
+
+        return(
+            <div style={{ textAlign: 'center'}}>
+                {/* Display Infomation Text */}
+                <h1>Hover To Increment / Decrement / Reset</h1>
+                {/* display count value */}
+                <h1>{count}</h1>
+
+            {/* Increment */}
+                <button onClick={increment} className='increment' onMouseEnter={mouseEnter}>Increment</button>
+            {/* Decrement */}
+                <button onClick={decrement} style={{ marginLeft : '10px'  }} className='decrement' onMouseEnter={mouseEnter}>Decrement</button>
+            {/* Reset */}   
+                <button onClick={reset} style={{ marginLeft : '20px'}} className='reset' onMouseEnter={mouseEnter}>Reset</button>
+
+            </div>
+        );
+}
+
+
+export default Counter;
