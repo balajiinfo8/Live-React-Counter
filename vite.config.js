8bf90618d1ce8d927cdba172e45c00cffbf24415
balajiinfo8 <vinothkumarbalaji150@gmail.com>
2025-11-03 14:43:50 +0530

    updated the project

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/React-Auto-Counter/',
+  base: '/Live-React-Counter/',
   plugins: [react()],
 });
 
