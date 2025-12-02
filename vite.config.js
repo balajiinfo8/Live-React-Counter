79d7ea430e148b75ddb3bd238e8334ad64c8ca3d
balajiinfo8 <vinothkumarbalaji150@gmail.com>
2025-12-02 20:50:24 +0530

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/Live-React-Counter/',
+  base: './', // relative path of staiic hosting 
   plugins: [react()],
 });
 
